39322e9c11823630bde854928803d175fafc92a3
Anatoly Kovalev <anatolko@yandex.ru>
2016-11-25 20:39:18 +0700

    разделение данных от шаблонов

--- js/game-2.js
+++ js/game-2.js
@@ -2,25 +2,51 @@ import getElementFromTemplate from './template';
 import display from './display';
 import showGame3 from './game-3';
 
-const game2Element = `<header class="header">
+const game2Data = {
+  header: {
+    timer: 'NN',
+    lives1: 'img/heart__empty.svg',
+    lives2: 'img/heart__full.svg',
+    lives3: 'img/heart__full.svg'
+  },
+  game: {
+    task: 'Угадай, фото или рисунок?',
+    content: 'http://placehold.it/705x455',
+    stats: {
+      result1: 'stats__result--wrong',
+      result2: 'stats__result--slow',
+      result3: 'stats__result--fast',
+      result4: 'stats__result--correct',
+      result5: 'stats__result--wrong',
+      result6: 'stats__result--unknown',
+      result7: 'stats__result--slow',
+      result8: 'stats__result--unknown',
+      result9: 'stats__result--fast',
+      result10: 'stats__result--unknown'
+    }
+  }
+};
+
+const header = `<header class="header">
   <div class="header__back">
     <span class="back">
       <img src="img/arrow_left.svg" width="45" height="45" alt="Back">
       <img src="img/logo_small.png" width="101" height="44">
     </span>
   </div>
-  <h1 class="game__timer">NN</h1>
+  <h1 class="game__timer">${game2Data.header.timer}</h1>
   <div class="game__lives">
-    <img src="img/heart__empty.svg" class="game__heart" alt="Life" width="32" height="32">
-    <img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">
-    <img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">
+    <img src="${game2Data.header.lives1}" class="game__heart" alt="Life" width="32" height="32">
+    <img src="${game2Data.header.lives2}" class="game__heart" alt="Life" width="32" height="32">
+    <img src="${game2Data.header.lives3}" class="game__heart" alt="Life" width="32" height="32">
   </div>
-</header>
-<div class="game">
-  <p class="game__task">Угадай, фото или рисунок?</p>
-  <form class="game__content  game__content--wide">
+</header>`;
+
+const task = `<p class="game__task">${game2Data.game.task}</p>`;
+
+const content = `<form class="game__content  game__content--wide">
     <div class="game__option">
-      <img src="http://placehold.it/705x455" alt="Option 1" width="705" height="455">
+      <img src="${game2Data.game.content}" alt="Option 1" width="705" height="455">
       <label class="game__answer  game__answer--photo">
         <input name="question1" type="radio" value="photo">
         <span>Фото</span>
@@ -30,23 +56,30 @@ const game2Element = `<header class="header">
         <span>Рисунок</span>
       </label>
     </div>
-  </form>
-  <div class="stats">
-    <ul class="stats">
-      <li class="stats__result stats__result--wrong"></li>
-      <li class="stats__result stats__result--slow"></li>
-      <li class="stats__result stats__result--fast"></li>
-      <li class="stats__result stats__result--correct"></li>
-      <li class="stats__result stats__result--wrong"></li>
-      <li class="stats__result stats__result--unknown"></li>
-      <li class="stats__result stats__result--slow"></li>
-      <li class="stats__result stats__result--unknown"></li>
-      <li class="stats__result stats__result--fast"></li>
-      <li class="stats__result stats__result--unknown"></li>
-    </ul>
-  </div>
-</div>`;
+  </form>`;
 
+const stats = `<div class="stats">
+    <ul class="stats">
+      <li class="stats__result ${game2Data.game.stats.result1}"></li>
+      <li class="stats__result ${game2Data.game.stats.result2}"></li>
+      <li class="stats__result ${game2Data.game.stats.result3}"></li>
+      <li class="stats__result ${game2Data.game.stats.result4}"></li>
+      <li class="stats__result ${game2Data.game.stats.result5}"></li>
+      <li class="stats__result ${game2Data.game.stats.result6}"></li>
+      <li class="stats__result ${game2Data.game.stats.result7}"></li>
+      <li class="stats__result ${game2Data.game.stats.result8}"></li>
+      <li class="stats__result ${game2Data.game.stats.result9}"></li>
+      <li class="stats__result ${game2Data.game.stats.result10}"></li>
+    </ul>
+  </div>`;
+
+const game2Element = `
+  ${header}
+  <div class="game">
+    ${task}
+    ${content}
+    ${stats}
+  </div>`;
 
 const moduleGame2 = getElementFromTemplate(game2Element);
 const gameAnswer2 = moduleGame2.querySelectorAll('.game__answer');
--- js/game-3.js
+++ js/game-3.js
@@ -2,49 +2,86 @@ import getElementFromTemplate from './template';
 import display from './display';
 import showStats from './stats';
 
-const game3Element = `<header class="header">
+const game3Data = {
+  header: {
+    timer: 'NN',
+    lives1: 'img/heart__empty.svg',
+    lives2: 'img/heart__full.svg',
+    lives3: 'img/heart__full.svg'
+  },
+  game: {
+    task: 'Найдите рисунок среди изображений',
+    content: {
+      option1: 'http://placehold.it/304x455',
+      option2: 'http://placehold.it/304x455',
+      option3: 'http://placehold.it/304x455'
+    },
+    stats: {
+      result1: 'stats__result--wrong',
+      result2: 'stats__result--slow',
+      result3: 'stats__result--fast',
+      result4: 'stats__result--correct',
+      result5: 'stats__result--wrong',
+      result6: 'stats__result--unknown',
+      result7: 'stats__result--slow',
+      result8: 'stats__result--unknown',
+      result9: 'stats__result--fast',
+      result10: 'stats__result--unknown'
+    }
+  }
+};
+
+const header = `<header class="header">
   <div class="header__back">
     <span class="back">
       <img src="img/arrow_left.svg" width="45" height="45" alt="Back">
       <img src="img/logo_small.png" width="101" height="44">
     </span>
   </div>
-  <h1 class="game__timer">NN</h1>
+  <h1 class="game__timer">${game3Data.header.timer}</h1>
   <div class="game__lives">
-    <img src="img/heart__empty.svg" class="game__heart" alt="Life" width="32" height="32">
-    <img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">
-    <img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">
+    <img src="${game3Data.header.lives1}" class="game__heart" alt="Life" width="32" height="32">
+    <img src="${game3Data.header.lives2}" class="game__heart" alt="Life" width="32" height="32">
+    <img src="${game3Data.header.lives3}" class="game__heart" alt="Life" width="32" height="32">
   </div>
-</header>
-<div class="game">
-  <p class="game__task">Найдите рисунок среди изображений</p>
-  <form class="game__content  game__content--triple">
+</header>`;
+
+const task = `<p class="game__task">${game3Data.game.task}</p>`;
+
+const content = `<form class="game__content  game__content--triple">
     <div class="game__option">
-      <img src="http://placehold.it/304x455" alt="Option 1" width="304" height="455">
+      <img src="${game3Data.game.content.option1}" alt="Option 1" width="304" height="455">
     </div>
     <div class="game__option  game__option--selected">
-      <img src="http://placehold.it/304x455" alt="Option 1" width="304" height="455">
+      <img src="${game3Data.game.content.option2}" alt="Option 1" width="304" height="455">
     </div>
     <div class="game__option">
-      <img src="http://placehold.it/304x455" alt="Option 1" width="304" height="455">
+      <img src="${game3Data.game.content.option3}" alt="Option 1" width="304" height="455">
     </div>
-  </form>
-  <div class="stats">
-    <ul class="stats">
-      <li class="stats__result stats__result--wrong"></li>
-      <li class="stats__result stats__result--slow"></li>
-      <li class="stats__result stats__result--fast"></li>
-      <li class="stats__result stats__result--correct"></li>
-      <li class="stats__result stats__result--wrong"></li>
-      <li class="stats__result stats__result--unknown"></li>
-      <li class="stats__result stats__result--slow"></li>
-      <li class="stats__result stats__result--unknown"></li>
-      <li class="stats__result stats__result--fast"></li>
-      <li class="stats__result stats__result--unknown"></li>
-    </ul>
-  </div>
-</div>`;
+  </form>`;
 
+const stats = `<div class="stats">
+    <ul class="stats">
+      <li class="stats__result ${game3Data.game.stats.result1}"></li>
+      <li class="stats__result ${game3Data.game.stats.result2}"></li>
+      <li class="stats__result ${game3Data.game.stats.result3}"></li>
+      <li class="stats__result ${game3Data.game.stats.result4}"></li>
+      <li class="stats__result ${game3Data.game.stats.result5}"></li>
+      <li class="stats__result ${game3Data.game.stats.result6}"></li>
+      <li class="stats__result ${game3Data.game.stats.result7}"></li>
+      <li class="stats__result ${game3Data.game.stats.result8}"></li>
+      <li class="stats__result ${game3Data.game.stats.result9}"></li>
+      <li class="stats__result ${game3Data.game.stats.result10}"></li>
+    </ul>
+  </div>`;
+
+const game3Element = `
+  ${header}
+  <div class="game">
+    ${task}
+    ${content}
+    ${stats}
+  </div>`;
 
 const moduleGame3 = getElementFromTemplate(game3Element);
 const gameAnswer3 = moduleGame3.querySelectorAll('.game__option');
